93ad1e6db5488bf63351b9fe82ef7b3287c270ee
Javed Habib <jh4official@gmail.com>
2023-08-13 23:37:35 +0530

    feat: Setup layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Footer from './components/Footer/Footer';
+import Grid from './components/Grid/Grid';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
     <>
-      <div className="w-screen bg-[--color-saffron]">1</div>
-      <div className="w-screen bg-[--color-white]">2</div>
-      <div className="w-screen bg-[--color-green]">3</div>
+      <div className="navbar h-[4rem] w-screen">
+        <Navbar />
+      </div>
+      <div className="flex justify-center items-center overflow-scroll h-[calc(100vh-8rem)]">
+        <Grid />
+      </div>
+      <div className="footer h-[4rem]">
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,5 @@
+function Grid() {
+  return <div>Grid</div>;
+}
+
+export default Grid;
