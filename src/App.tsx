3890469acd96c8992bee4a2611d13afc8dd4f436
Javed Habib <jh4official@gmail.com>
2023-08-13 23:31:18 +0530

    feat: Add husky, prettier and lintstaged

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-	return (
-		<>
-			<div className='w-screen bg-[--color-saffron]'>1</div>
-			<div className='w-screen bg-[--color-white]'>2</div>
-			<div className='w-screen bg-[--color-green]'>3</div>
-		</>
-	);
+  return (
+    <>
+      <div className="w-screen bg-[--color-saffron]">1</div>
+      <div className="w-screen bg-[--color-white]">2</div>
+      <div className="w-screen bg-[--color-green]">3</div>
+    </>
+  );
 }
 
 export default App;
