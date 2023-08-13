fd21ab81d1d88ca1d0d6d5525fec3b002460ad4a
Aditya062003 <adityavinay@iitbhilai.ac.in>
2023-08-14 01:02:22 +0530

    feat: Add navbar, footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,18 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="bg-gray-800 text-center py-6 text-white">
+        {' '}
+        Made with ♥ by{' '}
+        <a
+          href="https://github.com/OpenLake"
+          style={{ textDecoration: 'None' }}
+        >
+          Openlake
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,42 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const PixelText = styled.span`
+  font-family: 'Press Start 2P', cursive;
+  font-size: 24px;
+`;
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const [stars, setStars] = useState(0);
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            'https://api.github.com/repos/JaeAeich/Chaty/stargazers',
+          );
+          const data = await response.json();
+          setStars(data.length);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchData();
+    } catch (error) {}
+  }, []);
+  return (
+    <div className="flex justify-between items-center px-8 bg-[#CFC7E9] shadow-md py-4">
+      <div className="text-center">
+        <PixelText className="text-center">Pixels</PixelText>
+      </div>
+      <a href="https://github.com/JaeAeich/Pixels">
+        <div className="flex gap-2 items-center">
+          <div>⭐</div>
+          <div>{stars} Stars</div>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default Navbar;
