cea5a9ceee864d688c432723c3e2b2cb6e7c2781
Javed Habib <jh4official@gmail.com>
2023-08-13 23:13:48 +0530

    feat(CSS): Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-	return <>Pixels</>;
+	return (
+		<>
+			<div className='w-screen bg-[--color-saffron]'>1</div>
+			<div className='w-screen bg-[--color-white]'>2</div>
+			<div className='w-screen bg-[--color-green]'>3</div>
+		</>
+	);
 }
 
 export default App;
