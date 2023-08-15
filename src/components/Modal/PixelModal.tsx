48aa545e037e85a9d68b31c9f4aa9835066714e3
Aditya062003 <adityavinay@iitbhilai.ac.in>
2023-08-15 11:04:13 +0530

    Fixed modal image distortion

--- src/components/Modal/PixelModal.tsx
+++ src/components/Modal/PixelModal.tsx
@@ -34,19 +34,21 @@ function PixelModal(prop: ExtendedGridItem) {
   }
 
   return (
-    <div>
+    <div className="mx-4">
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
       >
-        <div className="meta flex justify-between items-center min-w-lg">
-          <div className="img h-[69px] w-[69px] rounded-full m-2 overflow-hidden">
-            <img src={pfp} alt={name} />
+        <div className="meta flex gap-4 justify-between items-center min-w-lg">
+          <div className="img md:h-[69px] md:w-[69px] h-[50px] w-[50px] rounded-full m-2 overflow-hidden">
+            <img className="w-full h-full" src={pfp} alt={name} />
           </div>
           <div className="text font-mono">
-            <div className="name text-xl">{name}</div>
-            <div className="message max-w-sm">{message}</div>
+            <div className="name text-sm md:text-xl">{name}</div>
+            <div className="text-sm md:text-xl max-w-xs md:max-w-sm">
+              {message}
+            </div>
           </div>
         </div>
       </Modal>
